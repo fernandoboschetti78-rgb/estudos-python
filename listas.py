812f2bbc0edc17ce2109372eb7103e9474e4258e
fernandoboschetti78-rgb <fernandoboschetti78@gmail.com>
2026-09-23 22:38:07 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,16 @@
+import time
+
+coiso = []
+coiso2=0
+
+while coiso2 != 3:
+    coiso.append(input("digite algo: "))
+    coiso2=coiso2+1
+
+print(coiso[0])
+time.sleep(1)
+
+print(coiso[1])
+time.sleep(1)
+
+print(coiso[2])
